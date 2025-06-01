38ed2e3ec044a438e38ed7673246cc91cfcf0c89
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-01 08:59:09 +0600

    Declared and printed the value of variable

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 class Hello {
     public static void main(String a[]) {
-        System.out.println("Hello World");
+        // System.out.println(3 + 5);
+        // System.out.println(8 + 5);
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2;
+
+        System.out.println(result);
     }
 }
