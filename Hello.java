6b1dd41d8c9bc79e015b74d907af3c073db174f4
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-06-01 14:00:42 +0600

    Exploring data types in java

--- Hello.java
+++ Hello.java
@@ -2,10 +2,12 @@ class Hello {
     public static void main(String a[]) {
         // System.out.println(3 + 5);
         // System.out.println(8 + 5);
-        int num1 = 3;
-        int num2 = 5;
-        int result = num1 + num2;
+        // int num1 = 3;
+        // int num2 = 5;
+        // int result = num1 + num2;
 
-        System.out.println(result);
+        double num1 = 6.5;
+
+        // System.out.println(result);
     }
 }
